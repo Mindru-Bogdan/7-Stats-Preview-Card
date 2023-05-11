83eae596581b19695f74d09ac311c6bdcb8fe67c
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-05-11 13:06:18 +0300

    Post initial files

---
+++ src/Components/Card.js
@@ -0,0 +1,20 @@
+const Card = () => {
+  return (
+    <div>
+      <h1>
+        Get <p>insights</p> that help your business grow.
+      </h1>
+      <p>
+        Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall
+        efficiency.
+      </p>
+      <div>
+        <h2>10k+</h2> <p>companies</p>
+        <h2>314</h2> <p>templates</p>
+        <h2>12m+</h2> <p>queries</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
